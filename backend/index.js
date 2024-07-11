2057c6a724afd084d80852e0fa3bd0c357b6f8c1
neeru0713 <neerurani1307@gmail.com>
2024-07-11 23:03:54 +0530

    env setup for local and production environment,health-check api

---
+++ backend/index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv");
+const path = require("path");
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json());
+
+// Load environment variables
+const envFile =
+  process.env.NODE_ENV === "production" ? ".env.production" : ".env";
+dotenv.config({ path: path.resolve(__dirname, envFile) });
+
+const mongoUrl = process.env.MONGODB_URL;
+
+// MongoDB connection
+mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
+
+// Health check endpoint
+app.get("/health-check", async (req, res) => {
+  try {
+    const mongoState = mongoose.connection.readyState;
+    const isConnected = mongoState === 1; 
+
+    if (isConnected) {
+      res.status(200).json({ message: "success" });
+    } else {
+      res.status(500).json({ message: "MongoDB connection down" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "service down", error: error.message });
+  }
+});
+
+const port = process.env.PORT || 3000; 
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
